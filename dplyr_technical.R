12bd93513c6df99e32c5eba1d2fac1cbe83ab842
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2020-03-18 20:03:30 -0500

    filter operations

--- dplyr_technical.R
+++ dplyr_technical.R
@@ -9,5 +9,15 @@ library(tidyverse)
 # mutate() : Create new variables with functions of existing variables
 # summarise() : collapse many values down to a single summary
 
+# to get all flights on day 1 of January
 
+filter(flights, month == 1, day == 1)
 
+# To get either in month of November or December
+
+filter(flights, month == 11 | month == 12)
+
+# In short hand
+
+nov_dec <- filter(flights, month %in% c(11,12))
+nov_dec
