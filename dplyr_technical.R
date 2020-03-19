f60471aefb84aea4ba88d653d1bfd2d40dab6c50
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2020-03-18 19:59:14 -0500

    dplyr learning

---
+++ dplyr_technical.R
@@ -0,0 +1,13 @@
+library(nycflights13)
+library(tidyverse)
+
+# Operations in dplyr
+
+# filter() : Pick observation by their value
+# arrange() : Reorder the rows
+# select() : Pick variables by their names
+# mutate() : Create new variables with functions of existing variables
+# summarise() : collapse many values down to a single summary
+
+
+
