5ba1f73b58b6bbf4d9a9e8d6e772ff311cb067a3
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2020-03-18 20:15:27 -0500

    updated some exercise too

--- dplyr_technical.R
+++ dplyr_technical.R
@@ -21,3 +21,28 @@ filter(flights, month == 11 | month == 12)
 
 nov_dec <- filter(flights, month %in% c(11,12))
 nov_dec
+
+# To filter the flights that were not delayed by more than two hours 
+
+filter(flights, !(arr_delay > 120 | dep_delay > 120))
+
+# To filter flights in air for more than 12 hours of 720 minutes and less than an hour or 60 minutes
+
+filter(flights, air_time >= 720 | air_time <= 60)
+
+# Exercise
+
+filter(flights, arr_delay < 120)
+
+filter(flights, dest == "IAH" | dest == "HOU")
+
+filter(flights, carrier == "UA" | carrier == "AA" | carrier == "DL")
+
+filter(flights, month == 7 | month == 8 | month == 9)
+
+
+
+
+
+
+
